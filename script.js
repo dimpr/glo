97f744b8e3a4c48d496c934762b57f696e5e0c59
dimpr <dmitri.designer@gmail.com>
2022-07-08 18:16:34 +0300

    урок №6 переделал

--- script.js
+++ script.js
@@ -1,57 +1,102 @@
-let userNum, botNum;
+// let userNum, botNum;
 
-const getRandom = function () {
-    let a = Math.floor(Math.random() * 100) + 1;
-    console.log("Бот загадал " + a);
-    return a;
-}
+// const getRandom = function () {
+//     let a = Math.floor(Math.random() * 100) + 1;
+//     console.log("Бот загадал " + a);
+//     return a;
+// }
 
-const getUserNum = function () {
-    let b = +prompt("Угадай число от 1 до 100", "");
-    console.log("Человек загадал " + b);
-    return b;
-}
+// const getUserNum = function () {
+//     let b = +prompt("Угадай число от 1 до 100", "");
+//     console.log("Человек загадал " + b);
+//     return b;
+// }
 
-const isNumber = function (num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
-}
+// const isNumber = function (num) {
+//     return !isNaN(parseFloat(num)) && isFinite(num);
+// }
 
-botNum = getRandom();
+// botNum = getRandom();
 
 
-const game = function (x) {
+// const game = function (x) {
 
-    userNum = getUserNum();
-    console.log(x);
+//     userNum = getUserNum();
+//     console.log(x);
 
-    if (isNumber(userNum)) {
+//     if (isNumber(userNum)) {
 
-        if (userNum === x) {
-            return alert("Поздравляю, Вы угадали!!!");
-        } else if (userNum > x) {
-            alert("Загаданное число меньше");
-            game(x);
-        } else if (userNum < x) {
-            alert("Загаданное число больше");
-            game(x);
-        } else if (userNum === null) {
+//         if (userNum === x) {
+//             return alert("Поздравляю, Вы угадали!!!");
+//         } else if (userNum > x) {
+//             alert("Загаданное число меньше");
+//             game(x);
+//         } else if (userNum < x) {
+//             alert("Загаданное число больше");
+//             game(x);
+//         } else if (userNum === null) {
+//             return alert("Игра окончена");
+//         }
+//     } else {
+//         alert("Введи число!");
+//         game(x);
+//     }
+// }
+
+// game(botNum);
+
+
+
+
+
+
+const game = function () {
+
+    let userNum,
+        botNum;
+
+    let getRandom = function () {
+        let a = Math.floor(Math.random() * 100) + 1;
+        console.log("Бот загадал " + a);
+        return a;
+    }
+
+    let getUserNum = function () {
+        let b = prompt("Угадай число от 1 до 100", "");
+        console.log("Человек загадал " + b);
+        return b;
+    }
+
+    let isNumber = function (num) {
+        return !isNaN(parseFloat(num)) && isFinite(num);
+    }
+
+    botNum = getRandom();
+    comparison();
+
+    function comparison() {
+
+        userNum = getUserNum();
+
+        if (userNum === null) {
             return alert("Игра окончена");
         }
-    } else {
-        alert("Введи число!");
-        game(x);
+        if (!isNumber(userNum)) {
+            comparison();
+        }
+        if (userNum === botNum) {
+            return alert("Поздравляю, Вы угадали!!!");
+        } else if (userNum > botNum) {
+            alert("Загаданное число меньше");
+            comparison();
+        } else if (userNum < botNum) {
+            alert("Загаданное число больше");
+            comparison();
+        }
+
     }
 }
 
-game(botNum);
 
+game();
 
-// Что должна делать программа:
-// — спрашивает пользователя: "Угадай число от 1 до 100".
-// — если пользовательское число больше, то бот выводит "Загаданное число меньше" и предлагает ввести новый вариант;
-// — если пользовательское число меньше, то бот выводит "Загаданное число больше" и предлагает ввести новый вариант;
-// — если пользователь ввел не число, то выводит сообщение "Введи число!" и предлагает ввести новый вариант;
-// — если пользователь нажимает "Отмена", то игра заканчивается и выводится сообщение "Игра окончена".
-// — если пользовательское число равно загаданному, то игра заканчивается и выводит сообщение "Поздравляю, Вы угадали!!!".
-// Программа должна быть выполнена с помощью рекурсии, без единого цикла.
-// Загаданное число должно храниться «в замыкании»
